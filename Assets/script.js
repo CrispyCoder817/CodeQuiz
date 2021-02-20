f046c800e563262886be6be13a87b6d74a7ea5dc
Brandon G.M <brandongmerritt@gmail.com>
2021-02-20 17:41:57 -0500

    Screenshots

--- Assets/script.js
+++ Assets/script.js
@@ -1,114 +1,121 @@
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
 
-let shuffledQuestions, currentQuestionIndex
+let shuffledQuestions, currentQuestionIndex;
 
-startButton.addEventListener('click', startGame)
-nextButton.addEventListener('click', () => {
-  currentQuestionIndex++
-  setNextQuestion()
-})
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
 
 function startGame() {
-  startButton.classList.add('hide')
-  shuffledQuestions = questions.sort(() => Math.random() - .5)
-  currentQuestionIndex = 0
-  questionContainerElement.classList.remove('hide')
-  setNextQuestion()
+  startButton.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove("hide");
+  setNextQuestion();
 }
 
 function setNextQuestion() {
-  resetState()
-  showQuestion(shuffledQuestions[currentQuestionIndex])
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
 function showQuestion(question) {
-  questionElement.innerText = question.question
-  question.answers.forEach(answer => {
-    const button = document.createElement('button')
-    button.innerText = answer.text
-    button.classList.add('btn')
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
     if (answer.correct) {
-      button.dataset.correct = answer.correct
+      button.dataset.correct = answer.correct;
     }
-    button.addEventListener('click', selectAnswer)
-    answerButtonsElement.appendChild(button)
-  })
+    button.addEventListener("click", selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
 }
 
 function resetState() {
-  clearStatusClass(document.body)
-  nextButton.classList.add('hide')
+  clearStatusClass(document.body);
+  nextButton.classList.add("hide");
   while (answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
   }
 }
 
 function selectAnswer(e) {
-  const selectedButton = e.target
-  const correct = selectedButton.dataset.correct
-  setStatusClass(document.body, correct)
-  Array.from(answerButtonsElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct)
-  })
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
   if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide')
+    nextButton.classList.remove("hide");
   } else {
-    startButton.innerText = 'Restart'
-    startButton.classList.remove('hide')
+    startButton.innerText = "Restart";
+    startButton.classList.remove("hide");
   }
 }
 
 function setStatusClass(element, correct) {
-  clearStatusClass(element)
+  clearStatusClass(element);
   if (correct) {
-    element.classList.add('correct')
+    element.classList.add("correct");
   } else {
-    element.classList.add('wrong')
+    element.classList.add("wrong");
   }
 }
 
 function clearStatusClass(element) {
-  element.classList.remove('correct')
-  element.classList.remove('wrong')
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
+
+function add1() {
+  alert("Adding 1 to your score!");
+  score = score + 1;
+  alert(score);
 }
 
 const questions = [
   {
-    question: 'Inside which HTML element do we put the JavaScript?',
+    question: "Inside which HTML element do we put the JavaScript?",
     answers: [
-      { text: '<script>', correct: true },
-      { text: '<scripting>', correct: false },
-      { text: '<js>', correct: false },
-      { text: '<javaScript>', correct: false }
-    ]
+      { text: "<script>", correct: true },
+      { text: "<scripting>", correct: false },
+      { text: "<js>", correct: false },
+      { text: "<javaScript>", correct: false },
+    ],
   },
 
   {
-    question: 'What is the correct syntax for referring to an external script called "xxx.js"?',
+    question:
+      'What is the correct syntax for referring to an external script called "xxx.js"?',
     answers: [
       { text: '<script name="xyz.js>', correct: false },
       { text: '<script src="xyz.js>', correct: true },
-      { text: '<script href="xyz.js>', correct: false }
-    ]
+      { text: '<script href="xyz.js>', correct: false },
+    ],
   },
   {
-    question: 'How do you create a function in JavaScript?',
+    question: "How do you create a function in JavaScript?",
     answers: [
-      { text: 'function:myFunction()', correct: false },
-      { text: 'function myFunction()', correct: true },
-      { text: 'function = myFunction()', correct: false }
-    ]
+      { text: "function:myFunction()", correct: false },
+      { text: "function myFunction()", correct: true },
+      { text: "function = myFunction()", correct: false },
+    ],
   },
   {
-    question: 'How does a FOR loop start?',
+    question: "How does a FOR loop start?",
     answers: [
-      { text: 'for (i <= 5; i++)', correct: false },
-      { text: 'for (i=0; i<=5; i++)', correct: true },
-      { text: 'for (i=1; i==5; I==)', correct: false }
-    ]
-  }
-]
+      { text: "for (i <= 5; i++)", correct: false },
+      { text: "for (i=0; i<=5; i++)", correct: true },
+      { text: "for (i=1; i==5; I==)", correct: false },
+    ],
+  },
+];
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CodeQuiz
 WebDev Project
+A quick and painful 4 question quiz that even stumbled the developer
+Screenshots: 
