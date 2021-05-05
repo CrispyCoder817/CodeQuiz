2d857137ab9848a34f4e68a5410b0d4260927ca3
Brandon G.M <brandongmerritt@gmail.com>
2021-05-05 13:08:06 -0400

    Array in JS

--- Assets/script.js
+++ Assets/script.js
@@ -117,40 +117,42 @@ const questions = [
   },
   {
     question: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: [
+    answers: [
       { text: "strings", correct: false },
       { text: "booleans", correct: false },
       { text: "numbers", correct: false },
-      { text: "alers", correct: true },
+      { text: "alerts", correct: true },
     ],
   },
   {
     question:
       "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "parentheses",
+    answers: [
+      { text: "quotes", correct: false },
+      { text: "curly brackets", correct: false },
+      { text: "parentheses", correct: true },
+      { text: "square brackets", correct: false },
+    ],
   },
   {
     question: "Arrays in JavaScript can be used to store ____.",
-    choices: [
-      "numbers and strings",
-      "other arrays",
-      "booleans",
-      "all of the above",
+    answers: [
+      { text: "Numbers & Strings", correct: false },
+      { text: "other arrays", correct: false },
+      { text: "booleans", correct: false },
+      { text: "all of the above", correct: true },
     ],
-    answer: "all of the above",
   },
   {
     question:
       "String values must be enclosed within ____ when being assigned to variables.",
     choices: ["commas", "curly brackets", "quotes", "parentheses"],
-    answer: "quotes",
+    answers: "quotes",
   },
   {
     question:
       "A very useful tool used during development and debugging for printing content to the debugger is:",
     choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
-    answer: "console.log",
+    answers: "console.log",
   },
 ];
